9e1995fc4513f80cc89ab284d11ab5838f92bcd3
An0nX <daniilsivalnev15@gmail.com>
2024-01-18 16:03:57 +0300

    Изменен шифр

--- userbot/src/encrypt.py
+++ userbot/src/encrypt.py
@@ -1,52 +1,74 @@
-import base64
-
 class CryptoUtils:
     @staticmethod
-    def encrypt(text: str, shift: int = 33) -> str:
+    def generate_vigenere_tableau():
         """
-        Encrypts a given text using the Caesar cipher algorithm.
+        Generates a Vigenère tableau that includes both letters and digits.
+        """
+        tableau = []
+        for i in range(36):
+            row = []
+            for j in range(36):
+                if 0 <= i < 26:
+                    if 0 <= j < 26:
+                        row.append(chr((i + j) % 26 + 65))
+                    else:
+                        row.append(str((j + i - 26) % 10))
+                else:
+                    if 0 <= j < 26:
+                        row.append(chr((j + i - 10) % 26 + 65))
+                    else:
+                        row.append(str((i + j - 36) % 10))
+            tableau.append(row)
+        return tableau
+
+    @staticmethod
+    def encrypt(text: str, keyword: str) -> str:
+        """
+        Encrypts a given text using the extended Vigenère cipher algorithm that includes digits.
 
         :param text: The text to encrypt.
-        :type text: str
-        :param shift: The number of positions to shift each character in the text. Default is 33.
-        :type shift: int
+        :param keyword: The keyword used for encryption.
         :return: The encrypted text.
-        :rtype: str
         """
+        tableau = CryptoUtils.generate_vigenere_tableau()
+        keyword_repeated = (keyword * (len(text) // len(keyword) + 1))[:len(text)]
         result = ""
 
-        for i in range(len(text)):
-            char = text[i]
-
-            if char.isupper():
-                result += chr((ord(char) + shift - 65) % 26 + 65)
+        for i, char in enumerate(text):
+            if char.isalpha() or char.isdigit():
+                text_index = ord(char.upper()) - 65 if char.isalpha() else ord(char) - 22
+                keyword_index = ord(keyword_repeated[i].upper()) - 65 if keyword_repeated[i].isalpha() else ord(keyword_repeated[i]) - 22
+                result += tableau[keyword_index][text_index]
             else:
-                result += chr((ord(char) + shift - 97) % 26 + 97)
+                result += char
 
         return result
 
     @staticmethod
-    def decrypt(text: str, shift: int = 33) -> str:
+    def decrypt(text: str, keyword: str) -> str:
         """
-        Decrypts the given text using the Caesar cipher algorithm by inversely shifting the characters.
+        Decrypts the given text using the extended Vigenère cipher algorithm that includes digits.
 
-        :param text: The text to be decrypted.
-        :type text: str
-        :param shift: The number of positions to shift each character in the text back. Default is 33.
-        :type shift: int
+        :param text: The text to decrypt.
+        :param keyword: The keyword used for decryption.
         :return: The decrypted text.
-        :rtype: str
         """
+        tableau = CryptoUtils.generate_vigenere_tableau()
+        keyword_repeated = (keyword * (len(text) // len(keyword) + 1))[:len(text)]
         result = ""
 
-        for i in range(len(text)):
-            char = text[i]
-
-            if char.isupper():
-                result += chr((ord(char) - shift - 65) % 26 + 65)
+        for i, char in enumerate(text):
+            if char.isalpha() or char.isdigit():
+                keyword_index = ord(keyword_repeated[i].upper()) - 65 if keyword_repeated[i].isalpha() else ord(keyword_repeated[i]) - 22
+                if char.isalpha():
+                    text_row = tableau[keyword_index]
+                    col_index = text_row.index(char.upper())
+                    result += chr(col_index + 65) if col_index < 26 else chr(col_index + 22)
+                else:
+                    text_row = tableau[keyword_index][26:]
+                    col_index = text_row.index(char)
+                    result += chr(col_index + 65) if col_index < 26 else chr(col_index + 22)
             else:
-                result += chr((ord(char) - shift - 97) % 26 + 97)
+                result += char
 
         return result
-
-  
