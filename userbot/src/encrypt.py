a6bcb23c1182fb7c061d46abcc929c7057b9ee2a
An0nX <daniilsivalnev15@gmail.com>
2024-01-18 15:50:21 +0300

    Тест

--- userbot/src/encrypt.py
+++ userbot/src/encrypt.py
@@ -28,14 +28,25 @@ class CryptoUtils:
     @staticmethod
     def decrypt(text: str, shift: int = 33) -> str:
         """
-        Decrypts the given text using the Caesar cipher algorithm.
+        Decrypts the given text using the Caesar cipher algorithm by inversely shifting the characters.
 
-        Parameters:
-            text (str): The text to be decrypted.
-            shift (int): The number of positions to shift each character in the text. Default is 33.
-
-        Returns:
-            str: The decrypted text.
+        :param text: The text to be decrypted.
+        :type text: str
+        :param shift: The number of positions to shift each character in the text back. Default is 33.
+        :type shift: int
+        :return: The decrypted text.
+        :rtype: str
         """
-        return CryptoUtils.encrypt(text, -shift)
+        result = ""
+
+        for i in range(len(text)):
+            char = text[i]
+
+            if char.isupper():
+                result += chr((ord(char) - shift - 65) % 26 + 65)
+            else:
+                result += chr((ord(char) - shift - 97) % 26 + 97)
+
+        return result
+
   
