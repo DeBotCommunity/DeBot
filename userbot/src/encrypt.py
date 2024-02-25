fe120d355b80a91e777a83c176fc477b90ed256c
An0nX <80145620+An0nX@users.noreply.github.com>
2024-02-25 12:09:16 +0300

    Update encrypt.py

--- userbot/src/encrypt.py
+++ userbot/src/encrypt.py
@@ -6,15 +6,15 @@ class CryptoUtils:
        """
        Encrypts a given text using base64 encoding.
        """
-       encoded_bytes = text
+       encoded_bytes = text.encode("utf-8")
        base64_bytes = base64.b64encode(encoded_bytes)
-       return base64_bytes
+       return base64_bytes.decode("utf-8")
 
    @staticmethod
    def decrypt(ciphertext: str) -> str:
        """
        Decrypts the given text using base64 decoding.
        """
-       base64_bytes = ciphertext
+       base64_bytes = ciphertext.encode("utf-8")
        decoded_bytes = base64.b64decode(base64_bytes)
-       return decoded_bytes
+       return decoded_bytes.decode("utf-8")
